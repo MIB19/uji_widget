d2e4746f6e80dbdf86b6bc9251e51566036c9912
mib19 <ivanbastomi@itsecasia.com>
2024-12-23 01:25:08 -0800

    fix index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Chatbot from "./chatbot.jsx";
 
-createRoot(document.getElementById("chatbot-uji")).render(
-  <StrictMode>
-    <Chatbot />
-  </StrictMode>
-);
+createRoot(document.getElementById("chatbot")).render(<Chatbot />);
