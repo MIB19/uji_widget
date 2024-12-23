9cf14d5a8bcc34ab7a0469ff26e876d2d8e1d327
mib19 <ivanbastomi@itsecasia.com>
2024-12-22 22:08:05 -0800

    run with root

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Chatbot from "./chatbot.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("chatbot-uji")).render(
   <StrictMode>
     <Chatbot />
   </StrictMode>
