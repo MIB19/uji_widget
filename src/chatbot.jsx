e4f090375529e12f9d7a5c1ce590e67a41db8409
mib19 <ivanbastomi@itsecasia.com>
2024-12-22 22:19:13 -0800

    fix chatbot

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ChatbotContainer from "./ChatbotContainer";
 
 function Chatbot() {
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    console.log(isOpen);
-  }, [isOpen]);
-
   return (
     <>
-      <div className="chatbot-button-container">
+      <div id="chatbot-uji" className="chatbot-button-container">
         <button
           style={{ color: "white", backgroundColor: "gray" }}
           onClick={() => setIsOpen((prev) => !prev)}
