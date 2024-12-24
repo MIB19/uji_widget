65b9e1a308fcd722a8a31265e40dea162cae427f
mib19 <ivanbastomi@itsecasia.com>
2024-12-23 22:11:25 -0800

    add tailwind

--- src/ChatbotContainer.jsx
+++ src/ChatbotContainer.jsx
@@ -1,13 +1,6 @@
 const ChatbotContainer = () => {
   return (
-    <div
-      className="chatbot-chat-container"
-      style={{
-        height: "120px",
-        width: "160px",
-        color: "black",
-      }}
-    >
+    <div className="cbt-fixed cbt-bottom-36 cbt-right-20 cbt-w-60 cbt-h-60 cbt-p-4 cbt-bg-gray-200 cbt-rounded-md">
       uji coba chatbot
     </div>
   );
--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -6,9 +6,9 @@ function Chatbot() {
 
   return (
     <>
-      <div id="chatbot-uji" className="chatbot-button-container">
+      <div id="chatbot-uji" className="cbt-fixed cbt-bottom-20 cbt-right-20">
         <button
-          style={{ color: "white", backgroundColor: "gray" }}
+          className="cbt-w-26 cbt-bg-gray-300 hover:cbt-bg-gray-600 cbt-py-3 cbt-px-10 cbt-rounded-full"
           onClick={() => setIsOpen((prev) => !prev)}
         >
           COBA Chatbot
